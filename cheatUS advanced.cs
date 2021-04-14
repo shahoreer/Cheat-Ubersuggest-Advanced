b497958c2648552d8f740a099ee51aee297d9267
shahoreer <81192173+shahoreer@users.noreply.github.com>
2021-04-14 12:54:02 -0400

    Update cheatUS advanced.cs

--- cheatUS advanced.cs
+++ cheatUS advanced.cs
@@ -1,5 +1,7 @@
 // Project: Cheat with ubersuggest 
 // 4-13-2021 (14 years now)
+// Start: 11:29 AM
+// End:   11:43 AM
 
 //You don't need to buy a pro version of ubersuggest for unlimited keyword research. 
 //Just put all of your keyword in the allkeyword arry.
@@ -9,6 +11,7 @@
 using System;
 class HelloWorld {
    static String[] allkeywords={"keyword_1_here","keyword_2_here","keyword_3_here"};// You can delate or add more keywords to it.
+       // for example: static String[] allkeywords={"how","to","make","money","online"};//
        // Dont't use any SPACE between keywords. Take another string in this arry and put your next keyword there.
   static void Main() {
       if(allkeywords.Length>0){
@@ -30,10 +33,10 @@ class HelloWorld {
       if(keywordslength>1){
           for(int i=0;i<keywordslength-1;i++){
           url+=allkeywords[i]+"%20";
-      }
+          }
         url+= allkeywords[keywordslength-1]+"&locId=2840&lang=en";
       }else{
-          url+=allkeywords[0]+="&locId=2840&lang=en";
+          url+=allkeywords[0]+"&locId=2840&lang=en";
       }
 
       return url;
